19038243b95c72efeffb2bd759b9e2f8c71c62dd
Anand Singh <anasingh@paymentus.com>
2024-05-23 17:55:47 -0400

    feat: add tokenization remote

--- api/controllers/DefaultController.js
+++ api/controllers/DefaultController.js
@@ -9,22 +9,23 @@
 const Controller = require('./Controller');
 const service = require('../services/DefaultService');
 const getToken = async (request, response) => {
-  if (!request.headers.authorization) {
-    return response.set('WWW-Authenticate', 'Basic').status(401).send();
-  }
-    // Get the Base64 encoded string that follows "Basic " in the Authorization header
-    const base64Credentials = request.headers.authorization.split(' ')[1];
+  // to-do add somesort of a model
+  // if (!request.headers.authorization) {
+  //   return response.set('WWW-Authenticate', 'Basic').status(401).send();
+  // }
+  //   // Get the Base64 encoded string that follows "Basic " in the Authorization header
+  //   const base64Credentials = request.headers.authorization.split(' ')[1];
 
-    // Decode the Base64 encoded string
-    const credentials = Buffer.from(base64Credentials, 'base64').toString('utf8');
+  //   // Decode the Base64 encoded string
+  //   const credentials = Buffer.from(base64Credentials, 'base64').toString('utf8');
   
-    // Split the decoded string into username and password
-    const [username, password] = credentials.split(':');
+  //   // Split the decoded string into username and password
+  //   const [username, password] = credentials.split(':');
   
-    // Check the username and password against process.env.GLOBAL_USER and process.env.GLOBAL_PASS
-    if (username !== process.env.GLOBAL_USER || password !== process.env.GLOBAL_PASS) {
-      return response.set('WWW-Authenticate', 'Basic').status(401).send();
-    }
+  //   // Check the username and password against process.env.GLOBAL_USER and process.env.GLOBAL_PASS
+  //   if (username !== process.env.GLOBAL_USER || password !== process.env.GLOBAL_PASS) {
+  //     return response.set('WWW-Authenticate', 'Basic').status(401).send();
+  //   }
   return Controller.handleGenerateAccessToken(request, response);
 };
 
