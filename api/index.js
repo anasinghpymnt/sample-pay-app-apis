d87c77c12860ee4d5a7416652f57995e01f6f4a0
Anand Singh <anasingh@paymentus.com>
2024-05-23 17:44:04 -0400

    feat: add tokenization remote

--- api/index.js
+++ api/index.js
@@ -1,21 +1,79 @@
-const config = require('./src/config');
-const logger = require('./src/logger');
-const ExpressServer = require('./src/expressServer');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const swaggerUI = require('swagger-ui-express');
+const jsYaml = require('js-yaml');
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const { OpenApiValidator } = require('express-openapi-validator');
+const logger = require('./logger');
+const config = require('./config');
+const app = express();
+try {
 
-const launchServer = async () => {
-  try {
-    // randomize port here 
-    const port = 8081;
-    this.expressServer = new ExpressServer(port, config.OPENAPI_YAML);
-    await this.expressServer.launch();
-    logger.info('Express server running');
-    return this.expressServer.getApp();
-  } catch (error) {
-    console.log(error);
-    logger.error('Express Server failure', error.message);
-  }
-};
-launchServer();
-// wait for 2 sec
+let schema;
 
-module.exports = this.expressServer.getApp();
+
+// Load OpenAPI schema
+try {
+  schema = jsYaml.safeLoad(fs.readFileSync( config.OPENAPI_YAML));
+} catch (e) {
+  logger.error('failed to load OpenAPI schema', e.message);
+}
+
+// Setup middleware
+app.use(cors());
+app.use(bodyParser.json({ limit: '14MB' }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Simple test route
+app.get('/hello', (req, res) => res.send(`Hello World. path: ${config.OPENAPI_YAML}`));
+
+// Serve OpenAPI document
+app.get('/openapi', (req, res) => res.sendFile(path.join(__dirname, 'api', 'openapi.yaml')));
+
+// Serve OpenAPI documentation UI
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(schema));
+
+app.get('/login-redirect', (req, res) => {
+  res.status(200).json(req.query);
+});
+
+app.get('/oauth2-redirect.html', (req, res) => {
+  res.status(200).json(req.query);
+});
+console.log('registered');
+
+// Initialize OpenApiValidator
+const openapiValidator = new OpenApiValidator({
+  apiSpec: config.OPENAPI_YAML,
+  ignorePaths: /.*\/token$/i,
+  validateRequests: false,
+  validateResponses: false,
+  operationHandlers: path.join(__dirname),
+  fileUploader: { dest: config.FILE_UPLOAD_PATH },
+});
+
+openapiValidator.install(app);
+
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message || err,
+    errors: err.errors || '',
+  });
+});
+
+// Start the server
+
+http.createServer(app).listen(config.URL_PORT);
+console.log(`Listening on port ${config.URL_PORT}`);
+} catch (e) {
+  console.error('Error starting server:', e);
+}
+
+module.exports = app;
